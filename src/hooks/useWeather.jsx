1862d4fc9ab9ed65265bf1ab41774d6770ca950f
sabbir <contact.sabbirbhuiyan@gmail.com>
2024-02-13 18:46:03 +0600

    udpate

--- src/hooks/useWeather.jsx
+++ src/hooks/useWeather.jsx
@@ -1,1 +1,49 @@
- 
+import { useState } from "react";
+
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    maxTemperature: "",
+    minTempareture: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    longitude: "",
+    latitude: "",
+  });
+
+  const [loading, setLoading] = useState({
+    state: false,
+    message: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = async () => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching weather data...",
+      });
+
+      const response = await fetch("");
+      if (!response.ok) {
+        const errorMessage = `Fetching weather data failed: ${response.status}`;
+        throw new Error(errorMessage);
+      }
+      const data = await response.json();
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading({
+        ...loading,
+        state: false,
+        message: "",
+      });
+    }
+  };
+};

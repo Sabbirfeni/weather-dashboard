d9fb22954ec8b0f7faa5f9f86eb1fa60b9e477fb
sabbir <contact.sabbirbhuiyan@gmail.com>
2024-02-13 15:51:54 +0600

    create custome hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 function App() {
   return (
-    <>
+    <div className="grid place-items-center h-screen">
       <Header />
-    </>
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
+    </div>
   );
 }
 
 export default App;
+
+// api key
+// 887ed62fd2cf41a090689225477a9074
